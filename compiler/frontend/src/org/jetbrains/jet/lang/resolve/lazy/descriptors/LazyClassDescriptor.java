497756e325b0363c51bd829f328511ff53a31196
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-09-26 13:13:24 -0700

    Forced resolution of all contents enhanced

--- compiler/frontend/src/org/jetbrains/jet/lang/resolve/lazy/descriptors/LazyClassDescriptor.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/resolve/lazy/descriptors/LazyClassDescriptor.java
@@ -50,6 +50,7 @@ import org.jetbrains.jet.lang.resolve.scopes.*;
 import org.jetbrains.jet.lang.types.JetType;
 import org.jetbrains.jet.lang.types.TypeConstructor;
 import org.jetbrains.jet.lang.types.TypeUtils;
+import org.jetbrains.jet.utils.WrappedValues;
 
 import java.util.*;
 
@@ -87,6 +88,8 @@ public class LazyClassDescriptor extends ClassDescriptorBase implements LazyDesc
     private final NotNullLazyValue<JetScope> scopeForClassHeaderResolution;
     private final NotNullLazyValue<JetScope> scopeForMemberDeclarationResolution;
     private final NotNullLazyValue<JetScope> scopeForPropertyInitializerResolution;
+
+    private final NullableLazyValue<Void> forceResolveAllContents;
 
     public LazyClassDescriptor(
             @NotNull ResolveSession resolveSession,
@@ -165,6 +168,13 @@ public class LazyClassDescriptor extends ClassDescriptorBase implements LazyDesc
                 return computeScopeForPropertyInitializerResolution();
             }
         });
+        this.forceResolveAllContents = storageManager.createRecursionTolerantNullableLazyValue(new Computable<Void>() {
+            @Override
+            public Void compute() {
+                doForceResolveAllContents();
+                return null;
+            }
+        }, null);
     }
 
     @Override
@@ -357,10 +367,14 @@ public class LazyClassDescriptor extends ClassDescriptorBase implements LazyDesc
 
     @Override
     public void forceResolveAllContents() {
+        forceResolveAllContents.compute();
+    }
+
+    private void doForceResolveAllContents() {
         getAnnotations();
-        getClassObjectDescriptor();
+        ForceResolveUtil.forceResolveAllContents(getClassObjectDescriptor());
         getClassObjectType();
-        getConstructors();
+        ForceResolveUtil.forceResolveAllContents(getConstructors());
         getContainingDeclaration();
         getThisAsReceiverParameter();
         getKind();
@@ -401,12 +415,19 @@ public class LazyClassDescriptor extends ClassDescriptorBase implements LazyDesc
                         }
                     }
                 },
+                new Computable<Object>() {
+                    @Override
+                    public Object compute() {
+                        return WrappedValues.unescapeExceptionOrNull(Collections.emptyList());
+                    }
+                },
                 new Consumer<Collection<JetType>>() {
                     @Override
                     public void consume(@NotNull Collection<JetType> supertypes) {
                         findAndDisconnectLoopsInTypeHierarchy(supertypes);
                     }
-                });
+                }
+        );
 
         private final NotNullLazyValue<List<TypeParameterDescriptor>> parameters = resolveSession.getStorageManager().createLazyValue(new Computable<List<TypeParameterDescriptor>>() {
             @Override
@@ -422,6 +443,15 @@ public class LazyClassDescriptor extends ClassDescriptorBase implements LazyDesc
                 return parameters;
             }
         });
+
+        private final NullableLazyValue<Void> forceResolveAllContents =
+                resolveSession.getStorageManager().createRecursionTolerantNullableLazyValue(new Computable<Void>() {
+                    @Override
+                    public Void compute() {
+                        doForceResolveAllContents();
+                        return null;
+                    }
+                }, null);
 
         @NotNull
         @Override
@@ -485,9 +515,13 @@ public class LazyClassDescriptor extends ClassDescriptorBase implements LazyDesc
 
         @Override
         public void forceResolveAllContents() {
+            forceResolveAllContents.compute();
+        }
+
+        private void doForceResolveAllContents() {
             getAnnotations();
-            getSupertypes();
-            getParameters();
+            ForceResolveUtil.forceResolveAllContents(getSupertypes());
+            ForceResolveUtil.forceResolveAllContents(getParameters());
         }
     }
 

8c70234a65fff5a084e92c2f7039bc7ab66fd618
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2013-09-30 13:49:51 +0400

    resolve delegated expression always using completer

--- compiler/frontend/src/org/jetbrains/jet/lang/resolve/DescriptorResolver.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/resolve/DescriptorResolver.java
@@ -1037,7 +1037,8 @@ public class DescriptorResolver {
             final JetExpression initializer = variable.getInitializer();
             if (initializer == null) {
                 if (hasDelegate && variableDescriptor instanceof PropertyDescriptor) {
-                    final JetExpression propertyDelegateExpression = ((JetProperty) variable).getDelegateExpression();
+                    final JetProperty property = (JetProperty) variable;
+                    final JetExpression propertyDelegateExpression = property.getDelegateExpression();
                     if (propertyDelegateExpression != null) {
                         return DeferredType.create(
                                 trace,
@@ -1046,7 +1047,7 @@ public class DescriptorResolver {
                                         new Function0<JetType>() {
                                             @Override
                                             public JetType invoke() {
-                                                return resolveDelegatedPropertyType((PropertyDescriptor) variableDescriptor, scope,
+                                                return resolveDelegatedPropertyType(property, (PropertyDescriptor) variableDescriptor, scope,
                                                                                     propertyDelegateExpression, dataFlowInfo, trace);
                                             }
                                         }));
@@ -1086,19 +1087,24 @@ public class DescriptorResolver {
 
     @NotNull
     private JetType resolveDelegatedPropertyType(
+            @NotNull JetProperty property,
             @NotNull PropertyDescriptor propertyDescriptor,
             @NotNull JetScope scope,
             @NotNull JetExpression delegateExpression,
             @NotNull DataFlowInfo dataFlowInfo,
             @NotNull BindingTrace trace
     ) {
-        JetType type = expressionTypingServices.safeGetType(scope, delegateExpression, TypeUtils.NO_EXPECTED_TYPE, dataFlowInfo, trace);
-
         JetScope accessorScope = JetScopeUtils.makeScopeForPropertyAccessor(propertyDescriptor, scope, this, trace);
-        JetType getterReturnType = delegatedPropertyResolver
-                .getDelegatedPropertyGetMethodReturnType(propertyDescriptor, delegateExpression, type, trace, accessorScope);
-        if (getterReturnType != null) {
-            return getterReturnType;
+
+        JetType type = delegatedPropertyResolver.resolveDelegateExpression(
+                delegateExpression, property, propertyDescriptor, scope, accessorScope, trace, dataFlowInfo);
+
+        if (type != null) {
+            JetType getterReturnType = delegatedPropertyResolver
+                    .getDelegatedPropertyGetMethodReturnType(propertyDescriptor, delegateExpression, type, trace, accessorScope);
+            if (getterReturnType != null) {
+                return getterReturnType;
+            }
         }
         return ErrorUtils.createErrorType("Type from delegate");
     }
